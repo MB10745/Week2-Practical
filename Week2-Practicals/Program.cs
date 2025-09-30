b06fb28298a86358f804a1b27ed4c05f542530ef
Mahnoor Baig <Baig-M4@ulster.ac.uk>
2025-09-30 11:29:22 +0300

    Add project files.

---
+++ Week2-Practicals/Program.cs
@@ -0,0 +1,9 @@
+﻿//Warm-Up Method
+double area = CalculateArea(5, 4);
+Console.WriteLine(area);
+
+static double CalculateArea(double length, double width)
+{
+    double area = length * width;
+    return area;
+}
